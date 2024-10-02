da65357fe7828497580dddcd1ff7cbda61c2ec76
tkhsksk <tkhsksk0318@gmail.com>
2024-10-02 18:55:02 +0900

    up

--- demo/settings/__init__.py
+++ demo/settings/__init__.py
@@ -1,6 +1,6 @@
 # settings/__init__.py
 
-from .settings import *                  # settings.py(本番環境用)をimport
+from .settings import *              # settings.py(本番環境用)をimport
 
 try:
     from .settings_dev import *      # settings_develop.py(開発環境用)をimport
--- demo/settings/settings_common.py
+++ demo/settings/settings_common.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'myapp.apps.MyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
