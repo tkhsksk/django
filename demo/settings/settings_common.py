7847a15a4bc0e696abd80a5ac2fcc53cd2250f58
tkhsksk <tkhsksk0318@gmail.com>
2024-10-03 10:56:46 +0900

    template
    テンプレートの追加

--- demo/settings/settings_common.py
+++ demo/settings/settings_common.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'demo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ str(BASE_DIR) + '/../templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+ 
+def home_template(request):
+    return render(request, 'home/index.html')
