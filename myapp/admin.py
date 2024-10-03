18b38601d32b2c087ca4797e7431ecd655d51f86
tkhsksk <tkhsksk0318@gmail.com>
2024-10-03 12:28:19 +0900

    form

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User # Userモデルをimport
 
 # Register your models here.
+
+admin.site.register(User) # Userモデルを使えるようにする
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_template, name='home_template'),
+    path('', views.home_template, name='home'),
 ]
